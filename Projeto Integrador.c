5e40b089b9a531c71e3e8caee2b7f229bf42b332
Adryan <100885847+adryanhy@users.noreply.github.com>
2022-05-09 20:08:50 -0300

    Update Projeto Integrador.c
    Adicionada a logica para a comparação de valores maior e menor

--- Projeto Integrador.c
+++ Projeto Integrador.c
@@ -2,36 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 #include <locale.h>
+#include <conio.h>
+
 int numero;
-int eqp_1;
-int eqp_2;
-int eqp_3;
-int eqp_4;
-int eqp_5;
 int num;
-
+float bat1[5];
+float bat2[5];
+float bat3[5];
 float calculo;
-
-eqp_1 = 1;
-eqp_2 = 2;
-eqp_3 = 3;
-eqp_4 = 4;
-eqp_5 = 5;
+float maior_tempo[5]; float media[5]; float total[5];
 
 int main()
 {
-    system("mode con:cols=100 lines=30");   //fun��o para configurar a tela do prompt
+    system("mode con:cols=100 lines=30");   //fun��o para configurar o prompt
     setlocale(LC_ALL, "Portuguese");
+
 int equipes[10];    //iniciando matriz para armazenar o valores das equipes
-int tempo[60];     //matriz para armazenar os tempos em min, seg, mseg
 int m;
 int seg;
 int mseg, i;
-    printf("                        ****************************************\n");
+    /*printf("                        ****************************************\n");
     printf("                        **************JHALB COMPANY*************\n");
     printf("                        ****************************************");
     sleep(3);
-    system("cls");
+    system("cls");*/
 
     printf("MENU PRINCIPAL - DIGITE A OPÇÃO DESEJADA\n\n");
     printf("1. Bateria 1\n");
@@ -54,6 +48,8 @@ int mseg, i;
             printf("Digite o tempo em milisegundos da equipe(%d): ",i+1);
             scanf("%d", &mseg  );
             calculo = (m*60)+seg+(mseg/100);
+            bat1[i] = calculo;
+            printf("%f", bat1[i]);
           }
 
             printf("\n6. Voltar ao menu principal:\n\n");
@@ -77,6 +73,7 @@ int mseg, i;
             printf("Digite o tempo em milisegundos da equipe(%d): ",i+1);
             scanf("%d", &mseg  );
             calculo = (m*60)+seg+(mseg/100);
+            bat2[i] = calculo;
           }
             printf("\n6. Voltar ao menu principal:\n\n");
             scanf("%d",&num);
@@ -97,6 +94,7 @@ int mseg, i;
             printf("Digite o tempo em milisegundos da equipe(%d): ",i+1);
             scanf("%d", &mseg  );
             calculo = (m*60)+seg+(mseg/100);
+            bat3[i] = calculo;
           }
 
             printf("\n6. Voltar ao menu principal:\n\n");
@@ -110,12 +108,29 @@ int mseg, i;
 
             case 4:
             system("cls");
+            for (i = 0; i < 5; i ++){
+            if(bat1[i] > bat2[i]){           //se bat1 na posição [i] for maior que bat2 na posição [i]
+                if(bat1[i] > bat3[i])       //se bat1 na posição [i] for maior que bat3 na posição [i]
+                maior_tempo[i] = bat1[i];  //entao maior_tempo recebe bat1[i]
+            else
+                maior_tempo[i] = bat3[i]; //senao maior_tempo recebe bat3[i]
+            }
+            else if (bat2[i] > bat3[i]){
+
+            maior_tempo[i]= bat2[i];
+            }
+            else {
+            maior_tempo[i] = bat3[i];
+            }
+            total[i] = (bat1[i] + bat2[i] + bat3[i]) - maior_tempo[i];
+            media[i] = total[i] / 2;
+            }
             printf("Obter classificação na tela\n\n");
-            printf("1 lugar: EQUIPE %d  \tTempo (%d:%d:%d)\n", num,tempo[0],tempo[1],tempo[2]);
-            printf("2 lugar: EQUIPE %d  \tTempo (minutos:segundos:mili-segundos):  xx:xx:xx \n", eqp_2);
-            printf("3 lugar: EQUIPE %d  \tTempo (minutos:segundos:mili-segundos):  xx:xx:xx \n", eqp_3);
-            printf("4 lugar: EQUIPE %d  \tTempo (minutos:segundos:mili-segundos):  xx:xx:xx \n", eqp_4);
-            printf("5 lugar: EQUIPE %d  \tTempo (minutos:segundos:mili-segundos):  xx:xx:xx \n", eqp_5);
+            printf("1 lugar: EQUIPE %f  \tTempo (%f:%d:%d)\n", bat1[i]);
+            printf("2 lugar: EQUIPE %d  \tTempo (minutos:segundos:mili-segundos):  xx:xx:xx \n" );
+            printf("3 lugar: EQUIPE %d  \tTempo (minutos:segundos:mili-segundos):  xx:xx:xx \n" );
+            printf("4 lugar: EQUIPE %d  \tTempo (minutos:segundos:mili-segundos):  xx:xx:xx \n" );
+            printf("5 lugar: EQUIPE %d  \tTempo (minutos:segundos:mili-segundos):  xx:xx:xx \n" );
             break;
 
             case 5:
@@ -131,7 +146,7 @@ int mseg, i;
             break;
 
             default:
-                printf("Op��o inv�lida, por favor escolha uma das opcoes acima!!\n");
+                printf("Opção invalida, por favor escolha uma das opcoes acima!!\n");
                 system("cls");}
 
 }
